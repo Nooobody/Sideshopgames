f60397b976dd5d8667f496d0e782aafa2307d8e3
Nooobody <thesilentcoder@outlook.com>
2016-11-06 12:56:15 +0200

    Hopefully fixing the database seed timeoutting.

--- app/database_seed.js
+++ app/database_seed.js
@@ -8,26 +8,27 @@ var config = {
     database: process.env.RDS_DATABASE
 };
 function seed_db() {
+    console.log("Starting to seed the database.");
     var client = new pg.Client(config);
     client.connect(function (err) {
         if (err)
             throw err;
-    });
-    client.query("CREATE TABLE players(\n      id SERIAL PRIMARY KEY,\n      steamid VARCHAR(30) not null,\n      name VARCHAR(40) not null,\n      exp BIGINT,\n      kills INTEGER,\n      wins INTEGER,\n      assists INTEGER,\n      deaths INTEGER,\n      total_games INTEGER,\n      buildings_built INTEGER,\n      buildings_razed INTEGER,\n      animals_killed INTEGER\n    )", function (err, result) {
-        if (err) {
-            console.log("Players already exist");
-        }
-        client.query("CREATE TABLE heroes_played(\n          player_id INTEGER PRIMARY KEY,\n          beastmaster SMALLINT,\n          gatherer SMALLINT,\n          hunter SMALLINT,\n          mage SMALLINT,\n          priest SMALLINT,\n          scout SMALLINT,\n          thief SMALLINT\n        )", function (err, result) {
+        client.query("CREATE TABLE players(\n        id SERIAL PRIMARY KEY,\n        steamid VARCHAR(30) not null,\n        name VARCHAR(40) not null,\n        exp BIGINT,\n        kills INTEGER,\n        wins INTEGER,\n        assists INTEGER,\n        deaths INTEGER,\n        total_games INTEGER,\n        buildings_built INTEGER,\n        buildings_razed INTEGER,\n        animals_killed INTEGER\n      )", function (err, result) {
             if (err) {
-                console.log("Heroes played already exist");
+                console.log("Players already exist");
             }
-            ;
-            var query = client.query("\n            CREATE TABLE player_friends(\n              player_id PRIMARY KEY\n              friend_id FOREIGN KEY\n            )", function (err, result) {
+            client.query("CREATE TABLE heroes_played(\n            player_id INTEGER PRIMARY KEY,\n            beastmaster SMALLINT,\n            gatherer SMALLINT,\n            hunter SMALLINT,\n            mage SMALLINT,\n            priest SMALLINT,\n            scout SMALLINT,\n            thief SMALLINT\n          )", function (err, result) {
                 if (err) {
-                    console.log("Friends already exist");
+                    console.log("Heroes played already exist");
                 }
                 ;
-                client.end();
+                var query = client.query("\n              CREATE TABLE player_friends(\n                player_id PRIMARY KEY\n                friend_id FOREIGN KEY\n              )", function (err, result) {
+                    if (err) {
+                        console.log("Friends already exist");
+                    }
+                    ;
+                    client.end();
+                });
             });
         });
     });
